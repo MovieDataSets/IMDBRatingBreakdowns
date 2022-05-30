93350d154da31d4c54109246a4c65ba6a4697dd1
DragonflyStats <kobriendublin@gmail.com>
2022-05-30 21:58:12 +0100

    Create RATINGS_BREAKDOWN_CODE.R

---
+++ Code/RATINGS_BREAKDOWN_CODE.R
@@ -0,0 +1,48 @@
+IMDBRatingBreakdowns
+
+library(tidyr)
+library(dplyr)
+library(magrittr)
+library(stringr)
+library(rvest)
+library(readr)
+
+getDATA <- function(html){
+  html %>%
+    html_nodes(".allText") %>%
+    html_text() %>%
+    str_trim() %>%
+    unlist()
+  
+}
+
+
+suppressMessages(IMDBdata <- read_csv("https://raw.githubusercontent.com/MovieDataSets/IMDBcodes/main/IMDBcode.csv?token=GHSAT0AAAAAABRM6EO4EBPEIG4FLDKZQVNWYUVF4YA")
+)
+
+
+CODES <- IMDBdata %>% pull(IMDBCode)
+
+MASTERDATA <- data.frame()
+
+
+for(i in 1:length(CODES)){
+
+IMDBCode <- CODES[i]
+
+THISURL <- paste0("http://www.imdb.com/title/",IMDBCode,"/ratings/")
+myURL <- read_html(THISURL)
+
+
+df_html <- getDATA(myURL)
+df_html <- data.frame(matrix(df_html[3:32],ncol=3,byrow=TRUE) )
+df_html <- data.frame(IMDBCode,DATE=Sys.Date(),df_html)
+df_html <- df_html %>% mutate(X3=gsub(",","",X3),X3=as.numeric(X3))
+
+MASTERDATA <- MASTERDATA %>% bind_rows(df_html)
+
+}
+
+MASTERDATA  %>% write.csv(paste0("C:/Users/kilbi/Documents/IMDB/Ratings-Breakdown-Data-",Sys.Date(),".csv"),row.names = FALSE)
+
+
